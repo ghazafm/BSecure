c2cf3be1ace16e826c7e203474ed82e41c0a0e30
haikal <haikal.gibran.gunawan@gmail.com>
2024-12-09 20:36:49 +0700

    Fake call

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,9 @@ dependencies {
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha03")
+
+
 }
