e65dea60595c1a588387b803305d6ae3f10bceeb
ghazafm <fauzan.ghaza@gmail.com>
2024-12-09 20:52:52 +0700

    merge

--- app/src/main/java/com/mawar/bsecure/MainActivity.kt
+++ app/src/main/java/com/mawar/bsecure/MainActivity.kt
@@ -29,6 +29,13 @@ import com.mawar.bsecure.ui.view.profile.ProfileScreen
 import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mawar.bsecure.ui.view.fakecall.TimerScreen
+import com.mawar.bsecure.ui.viewModel.fakecall.TimerViewModel
+import com.mawar.bsecure.ui.view.fakecall.IncomingCallScreen
 
 class MainActivity : ComponentActivity() {
     private lateinit var loginModel: LoginModel
@@ -76,6 +83,23 @@ class MainActivity : ComponentActivity() {
             BSecureTheme {
                 AppNavigation(loginModel, authRepository, googleSignInLauncher,userDetailsState,phoneAuthRepository)
             }
+            MyApp()
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    val timerViewModel: TimerViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = "timer_screen") {
+        composable("timer_screen") {
+            TimerScreen(navController = navController, timerViewModel = timerViewModel)
+        }
+        composable("incoming_call/{contactName}") { backStackEntry ->
+            val contactName = backStackEntry.arguments?.getString("contactName") ?: "Nama Tidak Diketahui"
+            IncomingCallScreen(contactName = contactName)
         }
     }
 }
