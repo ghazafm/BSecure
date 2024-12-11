cb697be22071459d01dc4481a59f0f1af425d261
haikal <haikal.gibran.gunawan@gmail.com>
2024-12-11 21:55:37 +0700

    AddCommunityUI

--- app/src/main/java/com/mawar/bsecure/ui/view/community/AddCommunity.kt
+++ app/src/main/java/com/mawar/bsecure/ui/view/community/AddCommunity.kt
@@ -2,19 +2,30 @@ package com.mawar.bsecure.ui.view.community
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -24,10 +35,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -36,75 +49,130 @@ import com.mawar.bsecure.R
 import com.mawar.bsecure.repository.CommunityRepository
 import com.mawar.bsecure.ui.viewModel.community.CommunityViewModel
 import com.mawar.bsecure.ui.viewModel.community.CommunityViewModelFactory
+import coil.compose.AsyncImage
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddCommunity(uid: String, navController: NavController, repository: CommunityRepository) {
     val viewModel: CommunityViewModel = viewModel(
         factory = CommunityViewModelFactory(repository)
     )
+    val communityViewModel: CommunityViewModel = viewModel(factory = CommunityViewModelFactory(CommunityRepository()))
+    viewModel.loadUser(uid)
+    val userData = communityViewModel.userCache[uid] ?: emptyMap()
     var contentCommunity by remember { mutableStateOf(TextFieldValue("")) }
     val coroutineScope = rememberCoroutineScope()
-    // Observe loading and error states from the ViewModel
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF5A2D82)) // Background color for the top part
-            .padding(top = 30.dp),
+            .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Text(
-            text = "Tambah ",
-            fontSize = 32.sp,
-            color = Color.White,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(80.dp))
-
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White, RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
-                .padding(horizontal = 32.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .background(Color(0xFF5A2D82))
+                .height(100.dp),
+            contentAlignment = Alignment.Center
         ) {
-            Spacer(modifier = Modifier.height(30.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.bsecure),
-                contentDescription = "Bsecure",
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(80.dp)
+                    .padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_keyboard_backspace_24),
+                        contentDescription = "Back",
+                        tint = Color.White,
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(0.65f))
+
+                Text(
+                    text = "Buat Postingan",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically // Untuk menyelaraskan elemen secara vertikal
+        ) {
+            // AsyncImage
+            AsyncImage(
+                model = userData?.get("profilePictureUrl"),
+                contentDescription = userData?.get("username") as? String ?: "User",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(Color.Gray, CircleShape),
+                contentScale = androidx.compose.ui.layout.ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = userData?.get("username") as? String ?: "User",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
             )
 
-            Spacer(modifier = Modifier.height(30.dp))
-
-            Divider(color = Color.Gray, thickness = 1.dp)
-
-            Spacer(modifier = Modifier.height(30.dp))
-
-            OutlinedTextField(
-                value = contentCommunity,
-                onValueChange = { contentCommunity = it },
-                label = { Text("Ketikkan isi konten") },
-                placeholder = { Text(".....") },
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.width(8.dp))
 
             Button(
                 onClick = {
-                    viewModel.addPost(uid= uid, contentCommunity.text)
+                    viewModel.addPost(uid = uid, contentCommunity.text)
                     navController.navigate("community/$uid")
                 },
             ) {
                 Text(text = "Kirim")
             }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically // Untuk menyelaraskan elemen secara vertikal
+        ) {
+            TextField(
+                value = contentCommunity,
+                onValueChange = { contentCommunity = it },
+                label = { Text("Apa yang sedang terjadi?") },
+                placeholder = { Text("Ketikkan disini...") },
+                modifier = Modifier
+                    .weight(1f),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    cursorColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+
+            )
+
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(30.dp))
+
+
 
             val isLoading by viewModel.isLoading.collectAsState()
             val errorMessage by viewModel.errorMessage.collectAsState()
@@ -115,7 +183,6 @@ fun AddCommunity(uid: String, navController: NavController, repository: Communit
             errorMessage?.let {
                 Text(text = it, color = Color.Red)
             }
-
         }
     }
 }
