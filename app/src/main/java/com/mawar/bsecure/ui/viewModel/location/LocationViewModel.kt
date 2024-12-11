f2cd6658916379c4262fd7f2eea812026c322058
hernandoatha <hernandoatha@student.ub.ac.id>
2024-12-12 03:33:16 +0700

    kodeerror

--- app/src/main/java/com/mawar/bsecure/ui/viewModel/location/LocationViewModel.kt
+++ app/src/main/java/com/mawar/bsecure/ui/viewModel/location/LocationViewModel.kt
@@ -11,10 +11,11 @@ import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 class LocationViewModel(application: Application) : AndroidViewModel(application) {
-
     private val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(application)
 
     private val _location = MutableStateFlow<LatLng?>(null)
@@ -22,12 +23,33 @@ class LocationViewModel(application: Application) : AndroidViewModel(application
 
     @SuppressLint("MissingPermission")
     fun updateLocation() {
-        viewModelScope.launch {
-            fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
-                location?.let {
-                    _location.value = LatLng(it.latitude, it.longitude)
-                }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                _location.value = LatLng(it.latitude, it.longitude)
             }
         }
     }
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        val locationRequest = LocationRequest.create().apply {
+            interval = 5000 // Interval pembaruan lokasi (5 detik)
+            fastestInterval = 2000 // Interval tercepat
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    for (location in locationResult.locations) {
+                        _location.value = LatLng(location.latitude, location.longitude)
+                    }
+                }
+            },
+            null // Handler untuk thread utama
+        )
+    }
 }
+
+
+
